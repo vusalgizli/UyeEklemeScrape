8b7f8ef89837f8a12b9e7e98c38d4e5b11628fc4
vusalgizli <102589016+vusalgizli@users.noreply.github.com>
2022-04-05 13:44:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ if not os.path.exists(f"Users/2056781888/phone.csv"):
    open(f"Users/2056781888/phone.csv","w")
 if not os.path.exists('data.csv'):
     open("data.csv","w")
-APP_ID = 3141462
-API_HASH = "fd6b9692c691a9c820fb3c4ed3e44c17"
-BOT_TOKEN = "2060259776:AAHM69mDvRSl7sC522fRynAoG-VmijWN9Ys"
+APP_ID = 16602040
+API_HASH = "a969618f49b2b2e0e33fc745e266bf17"
+BOT_TOKEN = "5177295008:AAGbGZmn8RnlUcf1z2RcCV03t2GBmw2Ojbo"
 UPDATES_CHANNEL = "InducedBots"
-OWNER= [1778272361,2056781888]
-PREMIUM=[1778272361,2056781888]
+OWNER= [5266934814,2056781888]
+PREMIUM=[5266934814,2056781888]
 app = pyrogram.Client("app", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 with open("data.csv", encoding='UTF-8') as f:
